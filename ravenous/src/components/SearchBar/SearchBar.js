9bd722c67a397f98ebe5f3dfe3efb1fd0794a5d3
lakshman416 <lakshmankumar416@gmail.com>
2019-09-28 18:51:28 -0700

    Updated Search Bar

--- ravenous/src/components/SearchBar/SearchBar.js
+++ ravenous/src/components/SearchBar/SearchBar.js
@@ -1,17 +1,34 @@
 import React from 'react';
 import './SearchBar.css';
 
-const sortByOptions = {
-    'Best Match':'best_match',
-    'Highest Rated':'rating',
-    'Most Reviewed':'review_count   '
-};
+
 
 class SearchBar extends React.Component{
+    constructor(props){
+        super(props);
+        this.state= {term:'',location:'',sortBy:'best_match'}
+        this.sortByOptions = {
+            'Best Match':'best_match',
+            'Highest Rated':'rating',
+            'Most Reviewed':'review_count'
+        };
+    }
+    getSortByClass(sortByOption){
+        if(sortByOption===this.state.sortBy){
+            return 'active';
+        }
+        else{
+            return '';
+        }
+        
+    }
+    handleSortByChanges(sortByOption){
+        this.setState({sortBy:sortByOption});
+    }
     renderSortByOptions(){
-        return Object.keys(sortByOptions).map(sortByOption=> {
-            let sortByOptionValue = sortByOptions[sortByOption];
-            return <li key = {sortByOptionValue}>{sortByOption}</li>
+        return Object.keys(this.sortByOptions).map(sortByOption=> {
+            let sortByOptionValue = this.sortByOptions[sortByOption];
+            return <li key = {sortByOptionValue} onClick={this.handleSortByChanges.bind(this, sortByOptionValue)} className={this.getSortByClass(sortByOptionValue)}>{sortByOption}</li>
         });
     }
     render(){
