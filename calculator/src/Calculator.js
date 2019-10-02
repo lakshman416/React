367327d62d40f72f5601635f2af0144dca30ca2d
lakshman416 <lakshmankumar416@gmail.com>
2019-10-02 16:20:22 -0700

    Optimized version 0.2

--- calculator/src/Calculator.js
+++ calculator/src/Calculator.js
@@ -12,51 +12,49 @@ class Calculator extends React.Component{
         }
         this.setInput1=this.setInput1.bind(this);
         this.setInput2=this.setInput2.bind(this);
-        this.add=this.add.bind(this);
-        this.sub=this.sub.bind(this);
-        this.mult=this.mult.bind(this);
-        this.div=this.div.bind(this);
+        this.calc=this.calc.bind(this);
     }
     setInput1(e){
+        var a = parseInt(e.target.value);
         this.setState(
             {
-                input1:parseInt(e.target.value)
+                input1:a
             })
                 }
     setInput2(e){
+        var b = parseInt(e.target.value);
         this.setState(
             {
-                input2:parseInt(e.target.value)
+                input2:b
             })
 
     }
-    add(){
-        let result= this.state.input1+this.state.input2;
-        let tempOut=`The calculation of ${this.state.input1}+${this.state.input2} = ${result}`;
-        let tempHist=this.state.history;
+    calc(operator){
+        var a=this.state.input1;
+        var b=this.state.input2;
+        var result, tempOut;
+
+        switch(operator){
+            case "add":
+                result = a+b;
+                tempOut=`The calculation of ${a}+${b} = ${result}`;
+                break;
+            case "sub":
+                result = a-b;
+                tempOut=`The calculation of ${a}-${b} = ${result}`;
+                break;
+            case "mult":
+                result = a*b;
+                tempOut=`The calculation of ${a}*${b} = ${result}`;
+                break;
+            case "div":
+                result = a/b;
+                tempOut=`The calculation of ${a}/${b} = ${result}`;
+                break;
+        }
+        var tempHist=this.state.history;
         tempHist.push(tempOut);
-        this.setState({output:tempOut,history:tempHist});
-    }
-    sub(){
-        let result= this.state.input1-this.state.input2;
-        let tempOut=`The calculation of ${this.state.input1}-${this.state.input2} = ${result}`;
-        let tempHist=this.state.history;
-        tempHist.push(tempOut);
-        this.setState({output:tempOut,history:tempHist});
-    }
-    mult(){
-        let result= this.state.input1*this.state.input2;
-        let tempOut=`The calculation of ${this.state.input1}*${this.state.input2} = ${result}`;
-        let tempHist=this.state.history;
-        tempHist.push(tempOut);
-        this.setState({output:tempOut,history:tempHist});   
-    }
-    div(){
-        let result= this.state.input1/this.state.input2;
-        let tempOut=`The calculation of ${this.state.input1}/${this.state.input2} = ${result}`;
-        let tempHist=this.state.history;
-        tempHist.push(tempOut)
-        this.setState({output:tempOut,history:tempHist});
+        this.setState({history:tempHist});
     }
     history(){
         return this.state.history.map(par=><li>{par}</li>);
@@ -68,10 +66,10 @@ class Calculator extends React.Component{
                 <input type="number" name="input1" id="input1" onChange={this.setInput1} />
                 <input type="number" name="input2" id="input2" onChange={this.setInput2} />
                 <br/>
-                <button id="add" onClick={this.add} >+</button>
-                <button id="sub" onClick={this.sub}>-</button>
-                <button id="mult" onClick={this.mult}>*</button>
-                <button id="div" onClick={this.div}>/</button>
+                <button id="add" onClick={()=>this.calc("add")} >+</button>
+                <button id="sub" onClick={()=>this.calc("sub")}>-</button>
+                <button id="mult" onClick={()=>this.calc("mult")}>*</button>
+                <button id="div" onClick={()=>this.calc("div")}>/</button>
                 <br/>
                 {/* <button id="getResult" >=</button> */}
                 
